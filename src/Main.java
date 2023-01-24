c35c9a3de5f40666c494c020a104547b5d94dd65
Denis_vrn <denisfedorov50@gmail.com>
2023-01-24 23:50:00 +0300

    My homework #10 is done.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         task1();
@@ -6,16 +8,32 @@ public class Main {
     }
     public static void task1() {
         System.out.println("Задание №1");
-        String firstName = "Ivan";
-        String middleName = "Ivanovich";
-        String lastName = "Ivanov";
+        System.out.println("Введите имя сотрудника");
+        Scanner name = new Scanner(System.in);
+        String firstName = name.nextLine();
+        System.out.println("Введите отчество сотрудника");
+        Scanner patronName = new Scanner(System.in);
+        String middleName = patronName.nextLine();
+        System.out.println("Введите фамилию сотрудника");
+        Scanner secondName = new Scanner(System.in);
+        String lastName = secondName.nextLine();
         String fullName = lastName + " " + firstName + " " + middleName;
         System.out.println("ФИО сотрудника — " + fullName);
         System.out.println();
     }
     public static void task2() {
         System.out.println("Задание №2");
-        String fullName = "Ivanov Ivan Ivanovich";
+        System.out.println("Задание №1");
+        System.out.println("Введите имя сотрудника");
+        Scanner name = new Scanner(System.in);
+        String firstName = name.nextLine();
+        System.out.println("Введите отчество сотрудника");
+        Scanner patronName = new Scanner(System.in);
+        String middleName = patronName.nextLine();
+        System.out.println("Введите фамилию сотрудника");
+        Scanner secondName = new Scanner(System.in);
+        String lastName = secondName.nextLine();
+        String fullName = lastName + " " + firstName + " " + middleName;
         System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
         System.out.println();
     }
