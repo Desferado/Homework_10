c92666c9999245d1d23aa58a6ee3904363220e03
Denis_vrn <denisfedorov50@gmail.com>
2023-01-24 23:19:55 +0300

    My homework #10

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        task1();
-        task2();
-        task3();
-    }
-    public static void task1() {
-        System.out.println("Задание №1");
-        String firstName = "Ivan";
-        String middleName = "Ivanovich";
-        String lastName = "Ivanov";
-        String fullName = lastName + " " + firstName + " " + middleName;
-        System.out.println("ФИО сотрудника — " + fullName);
-        System.out.println();
-    }
-    public static void task2() {
-        System.out.println("Задание №2");
-        String fullName = "Ivanov Ivan Ivanovich";
-        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
-        System.out.println();
-    }
-    public static void task3() {
-        System.out.println("Задание №3");
-        String fullName = "Иванов Семён Семёнович";
-        String fullName1 = fullName.replace('ё', 'е');
-        System.out.println("Данные ФИО сотрудника — " + fullName1);
+        System.out.println("Hello world");
     }
 }
